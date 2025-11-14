df0dd3dc92e5b42a1e21535345755a94262884cc
Agon Gjonbalaj <agon.gjonbalaj@gmail.com>
2025-11-14 11:57:44 +0100

    use the studio cloud-based service

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,7 @@
-// // https://nuxt.com/docs/api/configuration/nuxt-config
-// export default defineNuxtConfig({
-//   modules: ['@nuxt/content'],
-//   compatibilityDate: '2025-07-15',
-//   devtools: { enabled: true },
-// })
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  extends: ['docus'],
+  modules: ['@nuxt/content'],
+  compatibilityDate: '2025-07-15',
+  devtools: { enabled: true }
+})
