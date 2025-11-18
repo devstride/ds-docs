ef70f8e1a9997f3e4e75eeeeb036de4dc539ef1d
Agon Gjonbalaj <agon.gjonbalaj@gmail.com>
2025-11-18 14:12:51 +0100

    navigation styles override test

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,5 +3,10 @@ export default defineNuxtConfig({
   extends: ['docus'],
   modules: ['@nuxt/content'],
   compatibilityDate: '2025-07-15',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+
+  // Add custom CSS to override navigation text truncation
+  css: [
+    '~/assets/css/navigation.css'
+  ]
 })
