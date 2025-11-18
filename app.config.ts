72dadb518b1052d1795d329beaa52b61b6a5aaf3
Agon Gjonbalaj <agon.gjonbalaj@gmail.com>
2025-11-18 16:55:38 +0100

    update theme

---
+++ app.config.ts
@@ -0,0 +1,13 @@
+export default defineAppConfig({
+  ui: {
+    colors: {
+      "primary": "blue",
+      "secondary": "green",
+      "success": "green",
+      "info": "blue",
+      "warning": "yellow",
+      "error": "red",
+      "neutral": "neutral"
+    }
+  }
+})
