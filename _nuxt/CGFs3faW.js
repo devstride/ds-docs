39dedc52b816680f3a950865bfc1ff0c167d9124
agonix <agon.gjonbalaj@gmail.com>
2025-11-26 19:29:41 +0000

    Deploying to gh-pages from @ devstride/ds-docs@72f7be706b87828248ed2852835c4717601e43ab 🚀

---
+++ _nuxt/CGFs3faW.js
@@ -0,0 +1,1 @@
+import{r as n}from"./9TA8u4aC.js";import a from"./D0Didk-L.js";import{z as s,H as t,i as e,w as i,W as p}from"./DYAP0lE8.js";import"./BN_7HF1G.js";const d={__name:"ProseWarning",setup(c){const o=s();return(r,f)=>(e(),t(a,{color:"warning",icon:p(o).ui.icons.warning},{default:i(()=>[n(r.$slots,"default",{mdcUnwrap:"p"})]),_:3},8,["icon"]))}};export{d as default};
