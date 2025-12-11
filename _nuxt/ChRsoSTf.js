6af5e017dba831bcef1a1d08f1cd0075c8800aef
agonix <agon.gjonbalaj@gmail.com>
2025-12-11 16:50:00 +0000

    Deploying to gh-pages from @ devstride/ds-docs@ad98be17647b2cd296cf1ae1b02c30c7af5dc09c 🚀

--- _nuxt/ChRsoSTf.js
+++
@@ -1,1 +0,0 @@
-import{r as s}from"./CYZz0Tf3.js";import t from"./RBo2nGpG.js";import{z as e,H as a,i as p,w as c,W as i}from"./CoT98b_r.js";import"./BN_7HF1G.js";const d={__name:"ProseTip",setup(n){const o=e();return(r,f)=>(p(),a(t,{color:"success",icon:i(o).ui.icons.tip},{default:c(()=>[s(r.$slots,"default",{mdcUnwrap:"p"})]),_:3},8,["icon"]))}};export{d as default};
